41354d61eb20656344d1e28b654f9192877ddfb9
Hardik Joshi <114323100+hardikjoshi746@users.noreply.github.com>
2025-09-12 23:49:45 -0400

    backend ready for production

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 5001;
+const port = process.env.PORT || 5001;
+const cors = require("cors");
 const apiRoutes = require("./routes/apiRoutes");
 const fileUpload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
@@ -8,6 +9,13 @@ const cookieParser = require("cookie-parser");
 app.use(express.json()); // to parse the incoming requests with the json data
 app.use(cookieParser()); // to parse the incoming requests with the cookies
 app.use(fileUpload()); // to parse the incoming requests with the files
+
+app.use(
+  cors({
+    origin: "https://shop-sphere-khaki.vercel.app", // your frontend URL
+    credentials: true,
+  })
+);
 
 app.get("/", async (req, res, next) => {
   res.json({ message: "API Running..." });
